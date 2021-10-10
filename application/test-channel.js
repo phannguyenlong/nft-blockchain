ce963fb2cdc636a87c2e27c8a6f00a1bc0ab5494
phannguyenlong <phannguyenlong0812@gmail.com>
2021-10-10 22:25:54 +0700

    update: new folder structure

---
+++ application/test-channel.js
@@ -0,0 +1,136 @@
+const yaml = require("js-yaml")
+const fs = require("fs")
+const shell = require('shelljs');
+const { chdir, cwd } = require('process');
+
+async function createChannel(orderer, peers) {
+    let file = fs.readFileSync(__dirname + "/../leopard-network/sample/configtx.yaml")
+    let yamlFile = yaml.load(file.toString())
+    
+    // build a profiles
+    let configYamlFile = { Profiles: {} }
+    configYamlFile.Profiles = { LeopardGenesis: {} }
+    let LeopardGenesis = configYamlFile.Profiles.LeopardGenesis
+
+    // set up Policies
+    LeopardGenesis.Policies = yamlFile.Profiles.TwoOrgsApplicationGenesis.Policies
+
+    // setup capalities
+    LeopardGenesis.Capabilities = { V2_0: true }
+
+    // set up orderer Organization
+    let ordererOrg = {
+        Name: `orderer.${orderer.getNormalizeOrg}`,
+        ID: `orderer.${orderer.getNormalizeOrg}.msp`,
+        MSPDir: `../../organizations/${orderer.getNormalizeChannel}/ordererOrganizations/${orderer.getNormalizeOrg}/msp`,
+        Policies: {
+            Readers: { Type: "Signature", Rule: `OR('orderer.${orderer.getNormalizeOrg}.msp.member')` },
+            Writers: { Type: "Signature", Rule: `OR('orderer.${orderer.getNormalizeOrg}.msp.member')` },
+            Admins: { Type: "Signature", Rule: `OR('orderer.${orderer.getNormalizeOrg}.msp.admin')` }
+        },
+        OrdererEndpoints: [ `localhost:${orderer.ordererPort}` ]
+    }
+
+    // set up Orderer part
+    yamlFile.Orderer.Addresses = [`localhost:${orderer.ordererPort}`]
+    yamlFile.Orderer.EtcdRaft = {
+        Consenters: [{
+            Host: 'localhost',
+            Port: orderer.ordererPort,
+            ClientTLSCert:
+                `../../organizations/${orderer.getNormalizeChannel}/ordererOrganizations/${orderer.getNormalizeOrg}/orderers/orderer-${orderer.getNormalizeOrg}/tls/server.crt`,
+            ServerTLSCert:
+                `../../organizations/${orderer.getNormalizeChannel}/ordererOrganizations/${orderer.getNormalizeOrg}/orderers/orderer-${orderer.getNormalizeOrg}/tls/server.crt`,
+        }]
+    }
+    LeopardGenesis.Orderer = yamlFile.Orderer
+    LeopardGenesis.Orderer.Capabilities = { V2_0: true }
+    LeopardGenesis.Orderer.Organizations = [ordererOrg]
+
+    // setup Application part
+    LeopardGenesis.Application = yamlFile.Application
+    LeopardGenesis.Application.Organizations = []
+
+    // add peer org to Application Organizations
+    let peerOrgName = ""
+    for (let i = 0; i < peers.length; i++) {
+        let peer = peers[i]
+        let peerOrg = {
+            Name: `${peer.getNormalizeOrg}.msp`,
+            ID: `${peer.getNormalizeOrg}.msp`,
+            MSPDir: `../../organizations/${peer.getNormalizeChannel}/peerOrganizations/${peer.getNormalizeOrg}/msp`,
+            Policies: {
+                Readers: { Type: 'Signature', Rule: `OR('${peer.getNormalizeOrg}.msp.admin', '${peer.getNormalizeOrg}.msp.peer', '${peer.getNormalizeOrg}.msp.client')` },
+                Writers: { Type: 'Signature', Rule: `OR('${peer.getNormalizeOrg}.msp.admin', '${peer.getNormalizeOrg}.msp.client')` },
+                Admins: { Type: 'Signature', Rule: `OR('${peer.getNormalizeOrg}.msp.admin', '${peer.getNormalizeOrg}.msp.peer')` },
+                Endorsement: { Type: 'Signature', Rule: `OR('${peer.getNormalizeOrg}.msp.peer')` }
+            }
+        }
+        peerOrgName = peerOrgName.concat(`${peer.getNormalizeOrg},`) // get name for sh script
+        LeopardGenesis.Application.Organizations.push(peerOrg)
+    }
+    peerOrgName =  peerOrgName.substring(0, peerOrgName.length - 1); // remove last ","
+
+    // wrtie to file
+    let filePath = __dirname + `/../leopard-network/channel-config/${orderer.getNormalizeChannel}`
+    if (!fs.existsSync(filePath)) { // if note create new
+        fs.mkdirSync(filePath, { recursive: true });
+    }
+    fs.writeFileSync(filePath + "/configtx.yaml", yaml.dump(configYamlFile, { lineWidth: -1 }))
+
+    // run shell
+    let oldPwd = __dirname // save old location
+    shell.env["PATH"] = __dirname + "/../bin/:" + shell.env["PATH"] // commennt this if already set env
+    shell.exec(`bash -c 'cd ../leopard-network/scripts; ./setupChannel.sh ${orderer.getNormalizeOrg} ${orderer.ordererPort} ${orderer.getNormalizeChannel} ${peerOrgName}; cd ../../application; pwd'`)
+    chdir(oldPwd) // then set it again to prevent error
+}
+
+class Organization {
+    constructor(orgName, caAdmin, caPassword, channelName, caPort) {
+        this.orgName = orgName
+        this.caAdmin = caAdmin
+        this.caPassword = caPassword
+        this.channelName = channelName
+        this.caPort = caPort
+        this.caOperationPort = `1${caPort}`
+    }
+
+    // use for getting org name like company.c
+    get getNormalizeOrg() {
+        return this.orgName.replace(" ", ".").toLowerCase();
+    }
+    
+    get getNormalizeChannel() {
+        return this.channelName.replace(" ", ".").toLowerCase()
+    }
+}
+
+class peerOrganization extends Organization {
+    constructor(orgName, caAdmin, caPassword, peerAdmin, peerPassword, channelName, portNumber) {
+        super(orgName, caAdmin, caPassword, channelName, portNumber)
+        this.peerAdmin = peerAdmin
+        this.peerPassword = peerPassword
+        this.peerPort = portNumber + 1
+        this.peerOperationPort = `1${this.peerPort}`
+        this.chainCodePort = this.peerPort + 1
+    }
+}
+
+class ordererOrganizations extends Organization {
+    constructor(orgName, caAdmin, caPassword, ordererAdmin, ordererPassword, channelName, portNumber) {
+        super(orgName, caAdmin, caPassword, channelName, portNumber)
+        this.ordererAdmin = ordererAdmin
+        this.ordererPassword = ordererPassword
+        this.ordererPort = portNumber + 1
+        this.ordererOperationPort = `1${this.ordererPort}`
+        this.ordererAdminPort = this.ordererPort + 1 // use by onsadmin
+    }
+}
+
+let orderer = new ordererOrganizations("Company C", 'ordererAdmin', 'ordererPassword', 'admin', 'password', 'channel1', 8054)
+let peers = [
+    new peerOrganization("Company A", 'admin', 'password', 'admin', 'password', 'channel1', 6054),
+    new peerOrganization("Company B", 'admin', 'password', 'admin', 'password', 'channel1', 7054)
+]
+
+createChannel(orderer, peers)
---
+++ application/test.js
@@ -0,0 +1,189 @@
+const yaml = require("js-yaml")
+const fs = require("fs")
+const shell = require('shelljs');
+const path = require("path");
+const { chdir, cwd } = require('process');
+
+/**
+ * Peer CA using 2 port:
+ *  - port: ca orderer listen address
+ *  - 1`port`: ca oredere operation address
+ * Peer Using 3 port:
+ *  - peerPort = port + 1: listen port for peer
+ *  - peerPort + 1: peer chaincode address
+ *  - 1`peerPort`:  Peer operation listenaddress
+ */
+async function creatPeerAndCA(organization, port, username, password, channel) {
+    organization = organization.replace(" ", ".").toLowerCase(); // normalize data
+    channel = channel.replace(" ", ".").toLowerCase(); // normalize data
+
+    let file = fs.readFileSync(__dirname + "/../leopard-network/sample/test-docker-compose-ca.yaml")
+    let yamlFile = yaml.load(file.toString());
+
+    // add volumn for peer
+    yamlFile.volumes[`peer.${organization}`] = null
+    
+    //=================For CA====================
+    let caDockerConfig = yamlFile.services["ca.server"]
+
+    // general config
+    caDockerConfig.container_name = `ca.${organization}` // set container name
+    caDockerConfig.volumes = [`../../organizations/${channel}/fabric-ca-server/ca-${organization}:/etc/hyperledger/fabric-ca-server`] // mount points arrays
+    caDockerConfig.command = `sh -c 'fabric-ca-server start -b ${username}:${password} -d'` // lauch with username and password
+    caDockerConfig.ports = [`${port}:${port}`, `1${port}:1${port}`]
+
+    // add enviroment variable to caConfig
+    let caEnvConfig = [
+      `FABRIC_CA_SERVER_CA_NAME=ca-${organization}`,
+      `FABRIC_CA_SERVER_PORT=${port}`,
+      `FABRIC_CA_SERVER_OPERATIONS_LISTENADDRESS=0.0.0.0:1${port}`,
+    ];
+    caDockerConfig.environment = caDockerConfig.environment.concat(caEnvConfig)
+
+    // save file
+    yamlFile.services[`ca.${organization}`] = caDockerConfig
+    delete yamlFile.services["ca.server"]
+
+    //=================For Peer====================
+    let peerDockerConfig = yamlFile.services["peer.server"]
+    let peerPort = port + 1 // peer run at port 1 higher than orderer
+
+    //general config
+    peerDockerConfig.container_name = `peer.${organization}`
+    peerDockerConfig.ports = [`${peerPort}:${peerPort}`, `1${peerPort}:1${peerPort}`] 
+
+    let peerEnvConfig = [
+        `CORE_PEER_ID=peer.${organization}`,
+        `CORE_PEER_ADDRESS=peer.${organization}:${peerPort}`,
+        `CORE_PEER_LISTENADDRESS=0.0.0.0:${peerPort}`,
+        `CORE_PEER_CHAINCODEADDRESS=peer.${organization}:${peerPort + 1}`, // chaincode at 1 higher port than peer
+        `CORE_PEER_CHAINCODELISTENADDRESS=0.0.0.0:${peerPort + 1}`,
+        `CORE_PEER_GOSSIP_EXTERNALENDPOINT=peer.${organization}:${peerPort}`,
+        `CORE_PEER_GOSSIP_BOOTSTRAP=peer.${organization}:${peerPort}`,
+        `CORE_PEER_LOCALMSPID=${organization}.msp`,
+        `CORE_OPERATIONS_LISTENADDRESS=0.0.0.0:1${peerPort}`,
+        `CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/fabric/msp/user/admin/msp`
+    ]
+    peerDockerConfig.environment = peerDockerConfig.environment.concat(peerEnvConfig)
+
+    let peerVolumn = [
+        `../../organizations/${channel}/peerOrganizations/${organization}/peers/peer-${organization}/msp:/etc/hyperledger/fabric/msp`,
+        `../../organizations/${channel}/peerOrganizations/${organization}/peers/peer-${organization}/tls:/etc/hyperledger/fabric/tls`,
+        `peer.${organization}:/var/hyperledger/production`
+    ]
+    peerDockerConfig.volumes = peerDockerConfig.volumes.concat(peerVolumn)
+    
+    // save file
+    yamlFile.services[`peer.${organization}`] = peerDockerConfig
+    delete yamlFile.services["peer.server"]
+
+    // save to file
+    let filePath = __dirname + `/../leopard-network/docker/${channel}`
+    if (!fs.existsSync(filePath)) { // if note create new
+        fs.mkdirSync(filePath, { recursive: true });
+    }
+    fs.writeFileSync(filePath + `/ca_peer-compose-${organization}.yaml`, yaml.dump(yamlFile, { lineWidth: -1 }))
+
+
+    // run shell
+    let oldPwd = __dirname // save old location
+    shell.env["PATH"] = __dirname + "/../bin/:" + shell.env["PATH"] // commennt this if alread set env
+    shell.exec(`bash -c 'cd ../leopard-network/scripts; ./upPeerAndCA.sh ${organization} ${username} ${password} ${port} peer${username} peer${password} ${channel}; cd ../../application; pwd'`)
+    chdir(oldPwd) // then set it again to prevent error
+}
+
+/**
+ * Ordere CA using 2 port:
+ *  - port: ca orderer listen address
+ *  - 1`port`: ca oredere operation address
+ * Orderer Using 3 port:
+ *  - ordererPort = port + 1: listen port for orderer
+ *  - ordererPort + 1: Ordere_admin port use for tool osnadmin
+ *  - 1`ordererPort`:  Orderer operation listenaddress
+ */
+async function createOrdererAndCA(organization, port, channelAdminUsername, channelAdminPassword, channel) {
+    organization = organization.replace(" ", ".").toLowerCase(); // normalize data
+    channel = channel.replace(" ", ".").toLowerCase(); // normalize data
+
+    let file = fs.readFileSync(__dirname + "/../leopard-network/sample/orderer-docker-compose.yaml")
+    let yamlFile = yaml.load(file.toString());
+
+    // add volumn for peer
+    yamlFile.volumes[`orderer.${organization}`] = null
+
+    //=================For CA====================
+    let caDockerConfig = yamlFile.services["ca.orderer"]
+
+    // general config
+    caDockerConfig.container_name = `ca.orderer.${organization}` // set container name
+    caDockerConfig.volumes = [`../../organizations/${channel}/fabric-ca-server/ca.orderer-${organization}:/etc/hyperledger/fabric-ca-server`] // mount points arrays
+    caDockerConfig.command = `sh -c 'fabric-ca-server start -b ${channelAdminUsername}:${channelAdminPassword} -d'` // lauch with username and password
+    caDockerConfig.ports = [`${port}:${port}`, `1${port}:1${port}`] // set port
+
+    // add enviroment variable to caConfig
+    let caEnvConfig = [
+      `FABRIC_CA_SERVER_CA_NAME=ca.orderer-${organization}`,
+      `FABRIC_CA_SERVER_PORT=${port}`,
+      `FABRIC_CA_SERVER_OPERATIONS_LISTENADDRESS=0.0.0.0:1${port}`,
+    ];
+    caDockerConfig.environment = caDockerConfig.environment.concat(caEnvConfig)
+
+    // save file
+    yamlFile.services[`ca.orderer.${organization}`] = caDockerConfig
+    delete yamlFile.services["ca.orderer"]
+
+    //=================For Orderer====================
+    let ordererDockerConfig = yamlFile.services["orderer.server"]
+    let ordererPort = port + 1
+
+    // general config
+    ordererDockerConfig.container_name = `orderer.${organization}` // set container name
+    ordererDockerConfig.ports = [
+      `${ordererPort}:${ordererPort}`,
+      `1${ordererPort}:1${ordererPort}`,
+      `${ordererPort + 1}:${ordererPort + 1}`, //  port for ORDERER admin listnet addres
+    ];
+
+    let ordererEnv = [
+        `ORDERER_GENERAL_LISTENADDRESS=0.0.0.0`,
+        `ORDERER_GENERAL_LISTENPORT=${ordererPort}`,
+        `ORDERER_GENERAL_LOCALMSPID=orderer.${organization}.msp`,
+        `ORDERER_ADMIN_LISTENADDRESS=0.0.0.0:${ordererPort + 1}`, // rememeber to connect this port when use osnadmin tool
+        `ORDERER_OPERATIONS_LISTENADDRESS=0.0.0.0:1${ordererPort}`
+    ]
+    ordererDockerConfig.environment = ordererDockerConfig.environment.concat(ordererEnv)
+
+    let ordererVolumn = [
+        `\${PWD}/../../channel-artifacts/${channel}/genesis_block.pb:/var/hyperledger/orderer/orderer.genesis.block`,  // use absolute path for prevent error from docker
+        `../../organizations/${channel}/ordererOrganizations/${organization}/orderers/orderer-${organization}/msp:/var/hyperledger/orderer/msp`,
+        `../../organizations/${channel}/ordererOrganizations/${organization}/orderers/orderer-${organization}/tls/:/var/hyperledger/orderer/tls`,
+        `orderer.${organization}:/var/hyperledger/production/orderer`
+    ]
+
+    ordererDockerConfig.volumes = ordererVolumn
+
+    yamlFile.services[`orderer.${organization}`] = ordererDockerConfig
+    delete yamlFile.services["orderer.server"]
+
+    // save to file
+    let filePath = __dirname + `/../leopard-network/docker/${channel}`
+    if (!fs.existsSync(filePath)) { // if note create new
+        fs.mkdirSync(filePath, { recursive: true });
+    }
+    fs.writeFileSync(filePath + `/orderer-compose-${organization}.yaml`, yaml.dump(yamlFile, { lineWidth: -1 }))
+
+    // run shell
+    let oldPwd = __dirname // old location
+    shell.env["PATH"] =  __dirname + "/../bin/:" + shell.env["PATH"] // commennt this if alread set env
+    shell.exec(`bash -c 'cd ../leopard-network/scripts; ./upOrdererAndCA.sh ${organization} ${channelAdminUsername} ${channelAdminPassword} ${port} orderer${channelAdminUsername} peer${channelAdminPassword} ${channel}; cd ../../application; pwd'`)
+    chdir(oldPwd) // then set it again to prevent error
+}
+
+// note: 1 organizationw will host orderer and 1 peer. Other org can host 1 peer only
+async function main() {
+    await creatPeerAndCA("Company A", 6054, 'admin', 'password', 'channel1')
+    await creatPeerAndCA("Company B", 7054, 'admin', 'password', 'channel1')
+    await createOrdererAndCA("Company C", 8054, 'ordererAdmin', 'ordererPassword', 'channel1')
+}
+
+main()
